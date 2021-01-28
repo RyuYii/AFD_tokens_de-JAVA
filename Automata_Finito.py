567dc2e10949c996a62bcd37efca4317f49615a5
RyuYii <ruddysurco30@gmail.com>
2021-01-28 07:47:41 -0400

    version 1.0

--- Automata_Finito.py
+++ Automata_Finito.py
@@ -7,31 +7,51 @@ class AFND:
 		self.estado_final = grafo['estado_final']
 
 	def checkLetter(self, actualState, letter):
-		print(self.transiciones[actualState].keys())
 		print('-'*50)
-		for value in self.transiciones[actualState].keys():
-			print(self.transiciones[actualState][value])
-			print()
-			if letter in self.transiciones[actualState][value]:
-				print('\t'+value)
-				return value
+		states = []
+		try:
+			for value in self.transiciones[actualState].keys():
+				if letter in self.transiciones[actualState][value]:
+					print('\t'+value)
+					states.append(value)
+			return states
+		except KeyError:
+			return states
+
+	def efe(self, pila, word):
+		if not pila :
+			return False, ''
 		else:
-			return ''
+			#print(pila)
+			#print('-'*50)
+			actualState = pila.pop()
+			for letter in range(len(word)):
+				NextState = self.checkLetter(actualState, word[letter])
+				#print(NextState)
+				if len(NextState) >= 2:
+					try:
+						return self.efe(NextState, word[letter+1:])
+					except:
+						return False, ''
+				elif len(NextState) == 1:
+					actualState = NextState[0]
+				elif len(NextState) == 0 and len(pila) != 0 : 
+					return self.efe(pila, word)
+				else:
+					actualState = self.estado_inicial
+					break
+			if actualState in self.estado_final.keys():
+				return True, actualState
+			else:
+				return False, ''
 
 	def checkword(self, word):
 		actualState = self.estado_inicial
-		for letter in word:
-			NextState = self.checkLetter(actualState, letter)
-			#print(NextState)
-			if not NextState == '':
-				 actualState = NextState
-				 #print(actualState)
-			else:
-				actualState = self.estado_inicial
-				break
-		print(self.estado_final.keys())
-		if actualState in self.estado_final.keys():
-			return(f'Palabra: \t{word} es un numero {self.estado_final[actualState]}\n')
+		val, NextState = self.efe(self.checkLetter(actualState, word[0]),word[1:])
+
+		print(f'ultimo {NextState}')
+		if val:
+			return(f'Palabra: \t{word} es un {self.estado_final[NextState]}\n')
 		else:
 			return(f'Palabra: \t{word} no aceptada\n')
 		
@@ -39,7 +59,8 @@ class AFND:
 	def isAccept(self, cadena=''):
 		cad = ''
 		if(type(cadena) == str):
-			cadena = cadena.split(' ')
+			cadena = cadena.rstrip().split()
+			print(cadena)
 		if cadena == '':
 			print('No data')
 		for word in cadena:	
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from Automata_Finito import AFD
+from Automata_Finito import AFND
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext
@@ -17,7 +18,7 @@ class Aplicacion():
         #self.raiz.geometry('600x500')
         self.cont = 0
         #self.raiz.resizable(width=False,height=False)
-        self.raiz.title('Ver info')
+        self.raiz.title('AFND')
         self.grafo = ''
 
 
@@ -33,13 +34,13 @@ class Aplicacion():
         #input para cadenas
         self.e1 = Entry(self.raiz) 
         self.e1.grid(row=1, column=1)
-        self.e1['width']=40
+        self.e1['width']=61
         
         
         self.tinfo = scrolledtext.ScrolledText(self.raiz)
         self.tinfo.grid(row=2, column=1) 
-        self.tinfo['width']=40
-        self.tinfo['height']=5 
+        self.tinfo['width']=60
+        self.tinfo['height']=15 
         '''    
         self.btnshow = Button(self.raiz, 
                                 text='Quit', 
@@ -73,9 +74,9 @@ class Aplicacion():
 
         cad = f'Test #{self.cont}\n\n'
         cad += f'entrada: {self.e1.get()}\n\n'
-        cad += 'salida: '+self.grafo.isAccept(self.e1.get())
+        cad += 'salida: \n'+self.grafo.isAccept(self.e1.get())
         self.tinfo.insert('1.0',f'{cad}\n\n')
-        print(self.grafo.isAccept('if'))
+        #print(self.grafo.isAccept('if'))
         self.e1.delete('0', END)
         self.cont+=1
 
@@ -102,7 +103,138 @@ def main():
         'estado_inical':'q0',
         'estado_final':['qf']
     }
-    grafo =  AFD(ejemplo)
+    # Ejemplo de Entrada 
+
+    ejemplo_RealEntero = {
+        'alfabeto':['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&'],
+        'estados':['q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11','q12','q13','q14','q15','q16','q17','q18','q19','q20','q21','q22','q23','q24','q25','q26','q27','q28','q29','q30','q31','q32','q33','q34','q35','q36','q37','q38','q39','q40','q41','q42','q43','q44','q45','q46','q47','q48','q49','q50','q51','q52','q53','q54','q55','q56','q57','q58','q59','q60','q61','q62','q63','q64','q65','q66','q67','q68','q69','q70','q71','q72','q73','q74','q75','q76','q77','q78','q79','q80','q81','q82','q83','q84','q85','q86','q87'],
+        'transiciones':{
+            'q0':{'q1':['/','+','*','-','|','&','^'],
+                'q2':['>'],
+                'q3':['<'],
+                'q4':['>'],
+                'q69':['e'],
+                'q6':['='],
+                'q7':['0','1','2','3','4','5','6','7','8','9','-'],
+                'q9':['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_'],
+                'q12':['t'],
+                'q19':['f'],
+                'q24':['m'],
+                'q28':['e'],
+                'q35':['d'],
+                'q41':['S'],
+                'q47':['b'],
+                'q54':['s'],
+                'q59':['i'],
+                'q62':['c'],
+                'q66':['n'],
+                'q84':['+'],
+                'q11':['-'],
+                },
+            'q1':{'q6':['=']},
+            'q2':{'q1':['>']},
+            'q3':{'q1':['<']},
+            'q4':{'q5':['>']},
+            'q5':{'q1':['>']},
+            'q6':{'q10':['_']},
+            'q7':{'q8':['.'],
+                'q7':['0','1','2','3','4','5','6','7','8','9']
+                },
+            'q8':{'q69':['0','1','2','3','4','5','6','7','8','9']},
+            'q9':{'q9':['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_','0','1','2','3','4','5','6','7','8','9','!','@','#','$','%','^','&'],
+                'q70':['_']
+                },
+            'q11':{'q83':['-']},
+            'q12':{'q13':['r'],
+                'q16':['h']},
+            'q13':{'q14':['u']},
+            'q14':{'q15':['e']},
+            'q15':{'q71':['_']},
+            'q16':{'q17':['i']},
+            'q17':{'q18':['s']},
+            'q18':{'q72':['_']},
+            'q19':{'q20':['a']},
+            'q20':{'q21':['l']},
+            'q21':{'q22':['s']},
+            'q22':{'q23':['e']},
+            'q23':{'q73':['_']},
+            'q24':{'q25':['a']},
+            'q25':{'q26':['i']},
+            'q26':{'q27':['n']},
+            'q27':{'q74':['_']},
+            'q28':{'q29':['x']},
+            'q29':{'q30':['t']},
+            'q30':{'q31':['e']},
+            'q31':{'q32':['n']},
+            'q32':{'q33':['d']},
+            'q33':{'q34':['s']},
+            'q34':{'q75':['_']},
+            'q35':{'q36':['o']},
+            'q36':{'q37':['u']},
+            'q37':{'q38':['b']},
+            'q38':{'q39':['l']},
+            'q39':{'q40':['e']},
+            'q40':{'q76':['_']},
+            'q41':{'q42':['t']},
+            'q42':{'q43':['r']},
+            'q43':{'q44':['i']},
+            'q44':{'q45':['n']},
+            'q45':{'q46':['g']},
+            'q46':{'q77':['_']},
+            'q47':{'q48':['o']},
+            'q48':{'q49':['o']},
+            'q49':{'q50':['l']},
+            'q50':{'q51':['e']},
+            'q51':{'q52':['a']},
+            'q52':{'q53':['n']},
+            'q53':{'q78':['_']},
+            'q54':{'q55':['w']},
+            'q55':{'q56':['i']},
+            'q56':{'q57':['t']},
+            'q57':{'q58':['c']},
+            'q58':{'q79':['h']},
+            'q79':{'q88':['_']},
+            'q59':{'q60':['n']},
+            'q60':{'q61':['t']},
+            'q61':{'q80':['_']},
+            'q62':{'q63':['a']},
+            'q63':{'q64':['s']},
+            'q64':{'q65':['e']},
+            'q65':{'q81':['_']},
+            'q66':{'q67':['e']},
+            'q67':{'q68':['w']},
+            'q68':{'q82':['_']},
+            'q83':{'q87':['_']}, 
+            'q84':{'q85':['+']},
+            'q85':{'q86':['_']},
+            'q69':{'q69':['0','1','2','3','4','5','6','7','8','9']},
+        },
+        'estado_inical':'q0',
+        'estado_final':{
+            'q7':'Numero Entero',
+            'q10':'Operador de Asignacion',
+            'q69':'Numero Real',
+            'q70':'Identificador',
+            'q71':'true_',
+            'q72':'this_',
+            'q73':'false_',
+            'q74':'main_',
+            'q75':'extends_',
+            'q76':'double_',
+            'q77':'String_',
+            'q78':'boolean_',
+            'q79':'switch',
+            'q80':'int',
+            'q81':'case_',
+            'q82':'new_',
+            'q86':'incremento',
+            'q87':'decremento'
+
+        }
+    }
+    
+    grafo =  AFND(ejemplo_RealEntero)
+
     app = Aplicacion()
     app.add_grafo(grafo)
     app.raiz.mainloop()
@@ -110,3 +242,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
